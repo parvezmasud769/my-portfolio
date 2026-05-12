56fadbd48f55d7fbf0603ce2db0f03e3301bcd6a
Masud Parvez <89123752+parvezmasud769@users.noreply.github.com>
2026-05-13 01:30:25 +0600

    Create ict.js

---
+++ ict.js
@@ -0,0 +1,12 @@
+const ictQuestions = [
+    { q: "কম্পিউটারের স্থায়ী স্মৃতি কোনটি?", options: ["ROM", "RAM", "Cache", "Buffer"], correct: 0 },
+    { q: "নিচের কোনটি ইনপুট ডিভাইস?", options: ["Monitor", "Printer", "Keyboard", "Speaker"], correct: 2 },
+    { q: "WWW এর পূর্ণরূপ কী?", options: ["World Wide Web", "World Whole Web", "Wide World Web", "Web Wide World"], correct: 0 },
+    { q: "১ গিগাবাইট সমান কত মেগাবাইট?", options: ["৫১২ MB", "১০২৪ MB", "১০০০ MB", "২০৪৮ MB"], correct: 1 },
+    { q: "সিপিইউ (CPU) এর পূর্ণরূপ কী?", options: ["Central Process Unit", "Central Processing Unit", "Control Process Unit", "Core Processing Unit"], correct: 1 },
+    { q: "আধুনিক কম্পিউটারের জনক কে?", options: ["চার্লস ব্যাবেজ", "অ্যালান টুরিং", "জন ভন নিউম্যান", "স্টিভ ওয়াজনিয়াক"], correct: 1 },
+    { q: "ইউটিউব কত সালে প্রতিষ্ঠিত হয়?", options: ["২০০৩", "২০০৪", "২০০৫", "২০০৬"], correct: 2 },
+    { q: "নিচের কোনটি একটি ব্রাউজার?", options: ["Google", "Facebook", "Chrome", "Windows"], correct: 2 },
+    { q: "ইমেইল এর জনক কে?", options: ["রে টমলিনসন", "বিল গেটস", "মার্ক জাকারবার্গ", "স্টিভ জবস"], correct: 0 },
+    { q: "কম্পিউটারের মস্তিষ্ক বলা হয় কোনটিকে?", options: ["RAM", "Hard Disk", "Monitor", "CPU"], correct: 3 }
+];
